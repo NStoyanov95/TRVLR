9f5df921823d1bf0a89e7ad24df6a30a4425263f
NStoyanov95 <nstoyanov9512@gmail.com>
2024-09-21 12:29:03 +0300

    Add responsive styles to the navigation menu

--- Client/src/components/Navigation.jsx
+++ Client/src/components/Navigation.jsx
@@ -1,11 +1,57 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 export const Navigation = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <nav className="flex justify-between items-baseline bg-gray-200 max-w-full shadow-lg">
             <div className="p-5">
                 <h3>TRVLR:</h3>
             </div>
-            <ul className="flex">
+
+            <button
+                className="md:hidden bg-black relative"
+                onClick={toggleMenu}
+            >
+                Menu
+            </button>
+
+            <ul
+                className={`md:hidden absolute flex flex-col gap-2 items-center right-0 top-16 mt-2 min-w-full bg-white shadow-lg rounded-s p-2 z-50 ${
+                    isOpen ? "block" : "hidden"
+                } space-y-2 mt-3`}
+            >
+                <li>
+                    <Link
+                        to="/"
+                        className="hover:font-bold cursor-pointer block"
+                    >
+                        Home
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        to="/about"
+                        className="hover:font-bold cursor-pointer block"
+                    >
+                        About Us
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        to="/contact"
+                        className="hover:font-bold cursor-pointer block"
+                    >
+                        Contacts
+                    </Link>
+                </li>
+            </ul>
+
+            <ul className="hidden md:flex">
                 <li className="p-5 hover:font-bold cursor-pointer">
                     <Link>Home</Link>
                 </li>
