38831c596ae17725105a550f03c4c8b8e22c5f7a
NStoyanov95 <nstoyanov9512@gmail.com>
2024-09-23 21:47:22 +0300

    Add shortcut buttons with dummy data

--- Client/src/components/Home.jsx
+++ Client/src/components/Home.jsx
@@ -6,10 +6,10 @@ export const Home = () => {
     return (
         <>
             <div
-                className="bg-cover bg-center w-full h-96"
+                className="bg-cover bg-center w-full h-64 md:h-96"
                 style={{ backgroundImage: `url(${caravan})` }}
             ></div>
-            <div className="md:w-4/5 h-auto m-auto p-3">
+            <div className="md:w-4/5 h-auto m-auto py-4 ">
                 <div className="md:flex grid-flow-row md:gap-2 md:-mt-10 lg:gap-9 justify-center">
                     <BestDestinationCard />
                     <BestDestinationCard />
@@ -21,9 +21,9 @@ export const Home = () => {
                 style={{ backgroundImage: `url(${banner})` }}
             >
                 <div className="absolute inset-0 bg-black bg-opacity-60"></div>
-                <div className="relative text-gray-50">
+                <div className="relative text-gray-50 md:w-4/5 m-auto">
                     <h3 className="text-center">Start Your Journey</h3>
-                    <p className="p-10">
+                    <p>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit.
                         Alias possimus, ducimus maiores sed nisi a quibusdam
                         voluptas. Earum sequi ipsam sit ratione aliquid,
@@ -34,6 +34,20 @@ export const Home = () => {
                         provident perspiciatis exercitationem nisi alias eaque
                         magnam odit officiis id consequatur.
                     </p>
+                </div>
+            </div>
+            <div className="md:flex justify-center md:w-4/5 rounded-lg md:gap-20 m-auto md:p-2 bg-white">
+                <div className="md:w-36 shadow-lg p-6 bg-gray-100 rounded-xl">
+                    <h5 className="text-center">Hotels</h5>
+                </div>
+                <div className="md:w-36 shadow-lg p-6 bg-gray-100 rounded-xl">
+                    <h5 className="text-center">Cruises</h5>
+                </div>
+                <div className="md:w-36 shadow-lg p-6 bg-gray-100 rounded-xl">
+                    <h5 className="text-center">Campings</h5>
+                </div>
+                <div className="md:w-36 shadow-lg p-6 bg-gray-100 rounded-xl">
+                    <h5 className="text-center">Beach</h5>
                 </div>
             </div>
         </>
