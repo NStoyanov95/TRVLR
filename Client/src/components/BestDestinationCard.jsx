05f48ec678bdcfede373cf8c500625e366c1d7fb
NStoyanov95 <nstoyanov9512@gmail.com>
2024-09-23 21:47:47 +0300

    Update css

--- Client/src/components/BestDestinationCard.jsx
+++ Client/src/components/BestDestinationCard.jsx
@@ -2,7 +2,7 @@ import bolata from "../assets/bolata.jpg";
 
 export const BestDestinationCard = () => {
     return (
-        <div className="md:max-w-xs h-auto bg-grey-600 text-black shadow-lg bg-gray-100">
+        <div className="md:max-w-xs h-auto bg-grey-600 text-black shadow-lg shadow-black bg-gray-100">
             <img className="w-full" src={bolata} alt="asd" />
             <div className="p-5">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight">
