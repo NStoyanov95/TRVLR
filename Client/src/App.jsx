5638b3642b31b8391af8b543b29e43695aa0d1a1
NStoyanov95 <nstoyanov9512@gmail.com>
2024-09-23 21:44:55 +0300

    Create footer component

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,13 +1,15 @@
 import "./App.css";
+import { Footer } from "./components/Footer";
 import { Home } from "./components/Home";
 import { Navigation } from "./components/Navigation";
 
 function App() {
     return (
-        <>
+        <div className="bg-gray-200">
             <Navigation />
             <Home />
-        </>
+            <Footer />
+        </div>
     );
 }
 
