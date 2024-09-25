d1dbf045da65044d8a7dd9e4a715b68f37ce25ad
NStoyanov95 <nstoyanov9512@gmail.com>
2024-09-25 17:52:02 +0300

    Add dynamic data for destination card

--- Client/src/components/BestDestinationCard.jsx
+++ Client/src/components/BestDestinationCard.jsx
@@ -1,17 +1,14 @@
 import bolata from "../assets/bolata.jpg";
 
-export const BestDestinationCard = () => {
+export const BestDestinationCard = ({ image, description, city, country }) => {
     return (
         <div className="md:max-w-xs h-auto  bg-grey-600 text-black shadow-lg shadow-black bg-gray-100 md:rounded-b-lg">
-            <img className="w-full" src={bolata} alt="asd" />
+            <img className="w-full" src={image} alt="asd" />
             <div className="p-5">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight">
-                    Noteworthy technology acquisitions 2021
+                    {city}, {country}
                 </h5>
-                <p className="mb-3 font-normal">
-                    Here are the biggest enterprise technology acquisitions of
-                    2021 so far, in reverse chronological order.
-                </p>
+                <p className="mb-3 font-normal">{description}</p>
             </div>
         </div>
     );
