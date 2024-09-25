065385af310bbb1bc1d18c030e94f16834763332
NStoyanov95 <nstoyanov9512@gmail.com>
2024-09-25 20:33:00 +0300

    Update responsive styles

--- Client/src/components/BestDestinationCard.jsx
+++ Client/src/components/BestDestinationCard.jsx
@@ -2,13 +2,15 @@ import bolata from "../assets/bolata.jpg";
 
 export const BestDestinationCard = ({ image, description, city, country }) => {
     return (
-        <div className="md:max-w-xs h-auto  bg-grey-600 text-black shadow-lg shadow-black bg-gray-100 md:rounded-b-lg">
-            <img className="w-full" src={image} alt="asd" />
+        <div className="md:max-w-xs h-auto  bg-grey-600 text-black shadow-black bg-gray-100">
+            <img className="md:h-52 w-full" src={image} alt="asd" />
             <div className="p-5">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight">
                     {city}, {country}
                 </h5>
-                <p className="mb-3 font-normal">{description}</p>
+                <p className="md:mb-11 font-normal text-gray-500">
+                    {description}
+                </p>
             </div>
         </div>
     );
