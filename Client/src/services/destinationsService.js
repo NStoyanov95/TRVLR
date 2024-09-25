f737876690d3d0ae2b4ee840712d01459fb31e27
NStoyanov95 <nstoyanov9512@gmail.com>
2024-09-25 17:51:45 +0300

    Create fetch last 3 destinations service

--- Client/src/services/destinationsService.js
+++ Client/src/services/destinationsService.js
@@ -1,4 +1,4 @@
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, limit, orderBy, query } from "firebase/firestore";
 import { db } from "../firebase";
 
 export const fetchDestinations = async () => {
@@ -11,6 +11,26 @@ export const fetchDestinations = async () => {
         return destinations;
     } catch (error) {
         console.error("Error fetching destinations:", error);
+        return [];
+    }
+};
+
+export const fetchLast3Destinations = async () => {
+    try {
+        const q = query(
+            collection(db, "destinations"),
+            orderBy("Date", "desc"),
+            limit(3)
+        );
+        const querySnapshot = await getDocs(q);
+        const destinations = [];
+        querySnapshot.forEach((doc) => {
+            destinations.push({ id: doc.id, ...doc.data() });
+        });
+
+        return destinations;
+    } catch (error) {
+        console.log(error);
         return [];
     }
 };
