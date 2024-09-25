5c2c2a48330dc2f696f7c82fecccaf8cb757c16f
NStoyanov95 <nstoyanov9512@gmail.com>
2024-09-25 16:32:44 +0300

    Edit css

--- Client/src/components/ShortcutButtons.jsx
+++ Client/src/components/ShortcutButtons.jsx
@@ -2,7 +2,7 @@
 export const ShortcutButtons = ({ image }) => {
     return (
         <div
-            className="md:w-64 h-56 shadow-lg m-auto bg-center bg-cover"
+            className="md:w-64 h-56 shadow-lg m-auto bg-center bg-cover "
             style={{ backgroundImage: `url(${image})` }}
         ></div>
     );
