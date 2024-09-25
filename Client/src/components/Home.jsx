f160743054fa9a1195d167290074162af5fb403d
NStoyanov95 <nstoyanov9512@gmail.com>
2024-09-25 17:11:23 +0300

    Update hero section styles

--- Client/src/components/Home.jsx
+++ Client/src/components/Home.jsx
@@ -19,11 +19,24 @@ export const Home = () => {
     return (
         <>
             <div
-                className="bg-cover bg-center w-full h-64 md:h-96"
+                className="relative bg-cover bg-center w-full h-64 md:h-96 lg:h-screen flex justify-center items-center"
                 style={{ backgroundImage: `url(${caravan})` }}
-            ></div>
+            >
+                <div className="absolute inset-0 bg-gradient-to-b from-black via-transparent to-black opacity-60"></div>
+
+                <div className="relative text-center px-4">
+                    <h2 className="text-white text-3xl md:text-5xl lg:text-6xl font-bold leading-tight tracking-wide animate-fade-in">
+                        Journey Beyond the Ordinary:
+                        <br /> Discover Destinations That Inspire
+                    </h2>
+
+                    <p className="mt-4 text-gray-200 text-lg md:text-xl lg:text-2xl opacity-90 animate-slide-up">
+                        Your adventure begins here. Explore now.
+                    </p>
+                </div>
+            </div>
             <div className="md:w-4/5 h-auto m-auto py-4 ">
-                <div className="md:flex grid-flow-row md:gap-2 md:-mt-10 lg:gap-9 justify-center">
+                <div className="md:flex grid-flow-row md:gap-2 lg:gap-9 justify-center">
                     <BestDestinationCard />
                     <BestDestinationCard />
                     <BestDestinationCard />
