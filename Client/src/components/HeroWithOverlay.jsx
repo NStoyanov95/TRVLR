0b5287150e0c612783d95fbe33042289635facd1
NStoyanov95 <nstoyanov9512@gmail.com>
2024-09-26 22:44:51 +0300

    Change hero section background

--- Client/src/components/HeroWithOverlay.jsx
+++ Client/src/components/HeroWithOverlay.jsx
@@ -1,10 +1,10 @@
-import headingBackground from "../assets/headingBackground.jpg";
+import caravan from "../assets/caravan.jpeg";
 
 export const HeroWithOverlay = ({ title }) => {
     return (
         <div
-            className="relative bg-cover bg-center w-full h-64 md:h-96  flex justify-center items-center text-white mb-5"
-            style={{ backgroundImage: `url(${headingBackground})` }}
+            className="relative bg-cover bg-center w-full h-64 md:h-96 flex justify-center items-center text-white mb-5 md:m-0"
+            style={{ backgroundImage: `url(${caravan})` }}
         >
             <div className="absolute inset-0 bg-black bg-opacity-60"></div>
             <div className="relative">
