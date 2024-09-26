a697e4ff39d0356d532544485afac2725d6efb21
NStoyanov95 <nstoyanov9512@gmail.com>
2024-09-26 22:45:05 +0300

    Update destination card styles

--- Client/src/components/DestinationCard.jsx
+++ Client/src/components/DestinationCard.jsx
@@ -1,10 +1,12 @@
 export const DestinationCard = ({ image, country, city }) => {
     return (
-        <div className="flex flex-col md:items-center shadow-md text-center">
-            <img src={image} alt="" className="h-72 md:w-80 md:h-96" />
-            <div className="md:py-3">
-                <h5 className="text-gray-500">{country}</h5>
-                <h3>{city}</h3>
+        <div className="md:p-2 shadow-xl bg-gray-50">
+            <div className="flex flex-col md:items-center text-center">
+                <img src={image} alt="" className="h-72 md:w-80 md:h-96" />
+                <div className="md:py-3">
+                    <h5 className="text-gray-500">{country}</h5>
+                    <h3>{city}</h3>
+                </div>
             </div>
         </div>
     );
