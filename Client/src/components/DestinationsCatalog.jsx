0a8cf5e6dd4d65445818c8bc412d5a929be03312
NStoyanov95 <nstoyanov9512@gmail.com>
2024-09-26 16:20:29 +0300

    Show destinations catalog with dynamic data

--- Client/src/components/DestinationsCatalog.jsx
+++ Client/src/components/DestinationsCatalog.jsx
@@ -1,3 +1,35 @@
+import { useEffect, useState } from "react";
+import { HeroWithOverlay } from "./HeroWithOverlay";
+import { fetchDestinations } from "../services/destinationsService";
+import { DestinationCard } from "./DestinationCard";
+
 export const DestinationsCatalog = () => {
-    return <h1>Destinations Catalog</h1>;
+    const [destinations, setDestinations] = useState([]);
+
+    useEffect(() => {
+        const getDestinations = async () => {
+            const fetchedDestinations = await fetchDestinations();
+            setDestinations(fetchedDestinations);
+        };
+
+        getDestinations();
+    }, []);
+
+    return (
+        <>
+            <HeroWithOverlay title={"Explore Destinations"} />
+            <div className="bg-white">
+                <div className="md:w-4/5 m-auto md:py-20 md:flex flex-wrap justify-center md:gap-10">
+                    {destinations.map((destination) => (
+                        <DestinationCard
+                            key={destination.id}
+                            image={destination.image}
+                            city={destination.city}
+                            country={destination.country}
+                        />
+                    ))}
+                </div>
+            </div>
+        </>
+    );
 };
