1e6ffd2e7d532f29f4bdd7a5072e07c61b63541b
NStoyanov95 <nstoyanov9512@gmail.com>
2024-10-15 21:36:34 +0300

    Css update

--- Client/src/components/ShortcutButtons.jsx
+++ Client/src/components/ShortcutButtons.jsx
@@ -2,10 +2,10 @@ export const ShortcutButtons = ({ image, description }) => {
     return (
         <div className="md:flex flex-col gap-1 shadow-xl md:p-1">
             <div
-                className="md:w-64 h-56 shadow-lg m-auto bg-center bg-cover "
+                className="md:w-64 min-h-56 shadow-lg m-auto bg-center bg-cover "
                 style={{ backgroundImage: `url(${image})` }}
             ></div>
-            <div className="text-center py-3">
+            <div className="text-center h-16 py-2 px-1">
                 <p>{description}</p>
             </div>
         </div>
