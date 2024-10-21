593f96c534201b5bc410d0b4ce5ed0c0ba028054
NStoyanov95 <nstoyanov9512@gmail.com>
2024-10-21 21:07:09 +0300

    Add destination details fetching using ID from U

--- Client/src/components/DestinationDetailsCard.jsx
+++ Client/src/components/DestinationDetailsCard.jsx
@@ -1,4 +1,22 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { fetchSingleDestination } from "../services/destinationsService";
+
 export const DestinationDetailsCard = () => {
+    const { destinationId } = useParams();
+    const [destination, setDestination] = useState({});
+
+    useEffect(() => {
+        const getDestination = async () => {
+            const fetchedDestination = await fetchSingleDestination(
+                destinationId
+            );
+        };
+        getDestination();
+    }, [destinationId]);
+
+    console.log(destination);
+
     return (
         <>
             <h1>Details Card</h1>
