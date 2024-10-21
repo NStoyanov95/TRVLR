ccef091d15d2c73d2e1e68d3bbb3dd68bc9cffc2
NStoyanov95 <nstoyanov9512@gmail.com>
2024-10-21 21:06:12 +0300

    Create fetch single destination service

--- Client/src/services/destinationsService.js
+++ Client/src/services/destinationsService.js
@@ -1,4 +1,12 @@
-import { collection, getDocs, limit, orderBy, query } from "firebase/firestore";
+import {
+    collection,
+    doc,
+    getDoc,
+    getDocs,
+    limit,
+    orderBy,
+    query,
+} from "firebase/firestore";
 import { db } from "../firebase";
 
 export const fetchDestinations = async () => {
@@ -12,6 +20,24 @@ export const fetchDestinations = async () => {
     } catch (error) {
         console.error("Error fetching destinations:", error);
         return [];
+    }
+};
+
+export const fetchSingleDestination = async (destinationId) => {
+    try {
+        const docRef = doc(db, "destinations", destinationId);
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+            console.log(docSnap.data());
+            return { id: docSnap.id, ...docSnap.data() };
+        } else {
+            console.log("No such document!");
+
+            return null;
+        }
+    } catch (error) {
+        console.error("Error fetching destination:", error);
+        return null;
     }
 };
 
