3fcd5363be314d98a572f6f25018a24d9244b812
NStoyanov95 <nstoyanov9512@gmail.com>
2024-11-15 20:54:04 +0200

    Create details card with dynamic data

--- Client/src/components/DestinationDetailsCard.jsx
+++ Client/src/components/DestinationDetailsCard.jsx
@@ -11,6 +11,7 @@ export const DestinationDetailsCard = () => {
             const fetchedDestination = await fetchSingleDestination(
                 destinationId
             );
+            setDestination(fetchedDestination);
         };
         getDestination();
     }, [destinationId]);
@@ -19,7 +20,28 @@ export const DestinationDetailsCard = () => {
 
     return (
         <>
-            <h1>Details Card</h1>
+            <div className="md:w-5/6 md:mt-5 m-auto bg-white p-2 shadow-xl">
+                <div>
+                    <img
+                        src={destination.image}
+                        alt=""
+                        className="h-96 w-full"
+                    />
+                    <div className="md:p-5">
+                        <div className="mb-2">
+                            <h3>{destination.city}</h3>
+                            <h4 className="text-gray-500">
+                                {destination.country}
+                            </h4>
+                        </div>
+                        <div className="text-center">
+                            <p className="text-left">
+                                {destination.description}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 };
